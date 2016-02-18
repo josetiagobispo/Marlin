2fb4f7e55c3464ae113cabb5f44d6ca73cf0242a
esenapaj <creation0004@gmail.com>
2016-02-19 06:59:09 +0900

    add support for 4th extruder

--- Marlin/pins_RADDS.h
+++ Marlin/pins_RADDS.h
@@ -41,6 +41,10 @@
 #define E3_DIR_PIN         33
 #define E3_ENABLE_PIN      37
 
+#define E4_STEP_PIN        29
+#define E4_DIR_PIN         27
+#define E4_ENABLE_PIN      31
+
 #define SDPOWER                 -1
 #define SDSS                     4
 #define LED_PIN                 -1
