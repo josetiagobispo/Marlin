a5d08612b04724bd647617de6a5806a547b322ac
esenapaj <creation0004@gmail.com>
2016-07-31 20:56:46 +0900

    Fix broken debug message for 32bit architecture
    but this causes compilation warning

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -4915,7 +4915,11 @@ inline void gcode_M111() {
     for (uint8_t i = 0; i < COUNT(debug_strings); i++) {
       if (TEST(marlin_debug_flags, i)) {
         if (comma++) SERIAL_CHAR(',');
-        serialprintPGM((char*)pgm_read_word(&(debug_strings[i])));
+        #ifdef __SAM3X8E__
+          serialprintPGM((char*)pgm_read_dword(&(debug_strings[i])));
+        #else
+          serialprintPGM((char*)pgm_read_word(&(debug_strings[i])));
+        #endif
       }
     }
   }
