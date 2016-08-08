29969cb37aa1cf7924e9837461d34a1072fd5b0a
esenapaj <creation0004@gmail.com>
2016-08-09 00:25:10 +0900

    Follow the PR #4554 (Repair SPI-pins, some other pins)

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -329,34 +329,6 @@
   #define SUICIDE_PIN -1
 #endif
 
-#ifdef __SAM3X8E__
-  // Available chip select pins for HW SPI are 4 10 52 77
-  #if (SDSS == 4) || (SDSS == 10) || (SDSS == 52) || (SDSS == 77)
-    #if (SDSS == 4)
-      #define SPI_PIN         87
-      #define SPI_CHAN         1
-    #elif (SDSS == 10)
-      #define SPI_PIN         77
-      #define SPI_CHAN         0
-    #elif (SDSS == 52) 
-      #define SPI_PIN         86
-      #define SPI_CHAN         2
-    #else
-      #define SPI_PIN         77
-      #define SPI_CHAN         0
-    #endif
-    #define SCK_PIN           76
-    #define MISO_PIN          74
-    #define MOSI_PIN          75
-  #else
-    #define SOFTWARE_SPI
-    #define SCK_PIN           52
-    #define MISO_PIN          50
-    #define MOSI_PIN          51
-  #endif
-  #define SS_PIN            SDSS // A.28, A.29, B.21, C.26, C.29
-#endif
-
 // Marlin needs to account for pins that equal -1
 #ifndef __SAM3X8E__
   #define marlinAnalogInputToDigitalPin(p) ((p) == -1 ? -1 : (p) + 0xA0)
@@ -549,53 +521,81 @@
 /**
  * Define SPI Pins: SCK, MISO, MOSI, SS
  */
-#if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
-  #define AVR_SCK_PIN  13
-  #define AVR_MISO_PIN 12
-  #define AVR_MOSI_PIN 11
-  #define AVR_SS_PIN   10
-#elif defined(__AVR_ATmega644__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__) || defined(__AVR_ATmega1284P__)
-  #define AVR_SCK_PIN  7
-  #define AVR_MISO_PIN 6
-  #define AVR_MOSI_PIN 5
-  #define AVR_SS_PIN   4
-#elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-  #define AVR_SCK_PIN  52
-  #define AVR_MISO_PIN 50
-  #define AVR_MOSI_PIN 51
-  #define AVR_SS_PIN   53
-#elif defined(__AVR_AT90USB1287__) || defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB646__) || defined(__AVR_AT90USB647__)
-  #if ENABLED(AT90USBxx_TEENSYPP_ASSIGNMENTS)
-    // Teensy pin assignments
-    #define AVR_SCK_PIN  21
-    #define AVR_MISO_PIN 23
-    #define AVR_MOSI_PIN 22
-    #define AVR_SS_PIN   20
+#ifdef __SAM3X8E__
+  // Available chip select pins for HW SPI are 4 10 52 77
+  #if (SDSS == 4) || (SDSS == 10) || (SDSS == 52) || (SDSS == 77)
+    #if (SDSS == 4)
+      #define SPI_PIN         87
+      #define SPI_CHAN         1
+    #elif (SDSS == 10)
+      #define SPI_PIN         77
+      #define SPI_CHAN         0
+    #elif (SDSS == 52) 
+      #define SPI_PIN         86
+      #define SPI_CHAN         2
+    #else
+      #define SPI_PIN         77
+      #define SPI_CHAN         0
+    #endif
+    #define SCK_PIN           76
+    #define MISO_PIN          74
+    #define MOSI_PIN          75
   #else
-    // Traditional pin assignments
-    #define AVR_SCK_PIN  9
-    #define AVR_MISO_PIN 11
-    #define AVR_MOSI_PIN 10
-    #define AVR_SS_PIN   8
+    #define SOFTWARE_SPI
+    #define SCK_PIN           52
+    #define MISO_PIN          50
+    #define MOSI_PIN          51
   #endif
-#elif defined(__AVR_ATmega1281__) || defined(__AVR_ATmega2561__)
-  #define AVR_SCK_PIN  10
-  #define AVR_MISO_PIN 12
-  #define AVR_MOSI_PIN 11
-  #define AVR_SS_PIN   16
-#endif
+  #define SS_PIN            SDSS // A.28, A.29, B.21, C.26, C.29
+#else // __SAM3X8E__
+  #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
+    #define AVR_SCK_PIN  13
+    #define AVR_MISO_PIN 12
+    #define AVR_MOSI_PIN 11
+    #define AVR_SS_PIN   10
+  #elif defined(__AVR_ATmega644__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__) || defined(__AVR_ATmega1284P__)
+    #define AVR_SCK_PIN  7
+    #define AVR_MISO_PIN 6
+    #define AVR_MOSI_PIN 5
+    #define AVR_SS_PIN   4
+  #elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    #define AVR_SCK_PIN  52
+    #define AVR_MISO_PIN 50
+    #define AVR_MOSI_PIN 51
+    #define AVR_SS_PIN   53
+  #elif defined(__AVR_AT90USB1287__) || defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB646__) || defined(__AVR_AT90USB647__)
+    #if ENABLED(AT90USBxx_TEENSYPP_ASSIGNMENTS)
+      // Teensy pin assignments
+      #define AVR_SCK_PIN  21
+      #define AVR_MISO_PIN 23
+      #define AVR_MOSI_PIN 22
+      #define AVR_SS_PIN   20
+    #else
+      // Traditional pin assignments
+      #define AVR_SCK_PIN  9
+      #define AVR_MISO_PIN 11
+      #define AVR_MOSI_PIN 10
+      #define AVR_SS_PIN   8
+    #endif
+  #elif defined(__AVR_ATmega1281__) || defined(__AVR_ATmega2561__)
+    #define AVR_SCK_PIN  10
+    #define AVR_MISO_PIN 12
+    #define AVR_MOSI_PIN 11
+    #define AVR_SS_PIN   16
+  #endif
 
-#ifndef SCK_PIN
-  #define SCK_PIN  AVR_SCK_PIN
-#endif
-#ifndef MISO_PIN
-  #define MISO_PIN AVR_MISO_PIN
-#endif
-#ifndef MOSI_PIN
-  #define MOSI_PIN AVR_MOSI_PIN
-#endif
-#ifndef SS_PIN
-  #define SS_PIN   AVR_SS_PIN
-#endif
+  #ifndef SCK_PIN
+    #define SCK_PIN  AVR_SCK_PIN
+  #endif
+  #ifndef MISO_PIN
+    #define MISO_PIN AVR_MISO_PIN
+  #endif
+  #ifndef MOSI_PIN
+    #define MOSI_PIN AVR_MOSI_PIN
+  #endif
+  #ifndef SS_PIN
+    #define SS_PIN   AVR_SS_PIN
+  #endif
+#endif // __SAM3X8E__
 
 #endif //__PINS_H
