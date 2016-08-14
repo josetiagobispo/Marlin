5f5131e67f73f95a37d7e38740dfcec117d9da79
esenapaj <creation0004@gmail.com>
2016-08-14 20:56:19 +0900

    Follow the commit b4af4441c5f0fa54508e3023bf5007bd98f41b86 (Clean up watchdog impl.)

--- Marlin/watchdog.cpp
+++ Marlin/watchdog.cpp
@@ -44,13 +44,6 @@ void watchdog_init() {
   #endif
 }
 
-#ifdef __SAM3X8E__
-  // Reset watchdog. MUST be called every 1s after init or avr will reset.
-  void watchdog_reset() {
-    WDT_Restart(WDT);
-  }
-#endif
-
 #ifndef __SAM3X8E__
   //===========================================================================
   //=================================== ISR ===================================
--- Marlin/watchdog.h
+++ Marlin/watchdog.h
@@ -28,23 +28,14 @@
   #include <avr/wdt.h>
 #endif
 
-#ifdef __SAM3X8E__
-  #if ENABLED(USE_WATCHDOG)
-    // Initialize watchdog with a 4 second interrupt time
-    void watchdog_init();
-    // pad the dog/reset watchdog. MUST be called at least every second after the first watchdog_init or AVR will go into emergency procedures..
-    void watchdog_reset();
-  #else
-    //If we do not have a watchdog, then we can have empty functions which are optimized away.
-    FORCE_INLINE void watchdog_init() {};
-    FORCE_INLINE void watchdog_reset() {};
-  #endif
-#else
-  // Initialize watchdog with a 4 second interrupt time
-  void watchdog_init();
+// Initialize watchdog with a 4 second interrupt time
+void watchdog_init();
 
-  // Reset watchdog. MUST be called at least every 4 seconds after the
-  // first watchdog_init or AVR will go into emergency procedures.
+// Reset watchdog. MUST be called at least every 4 seconds after the
+// first watchdog_init or AVR will go into emergency procedures.
+#ifdef __SAM3X8E__
+  inline void watchdog_reset() { WDT_Restart(WDT); }
+#else
   inline void watchdog_reset() { wdt_reset(); }
 #endif
 
