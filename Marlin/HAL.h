88129c470027355642da8ff3262e8c8a3301fecb
esenapaj <creation0004@gmail.com>
2016-08-16 02:32:37 +0900

    Increase interrupt frequency of Temperature::isr() on Due from 2kHz to 4kHz experimentarlly

--- Marlin/HAL.h
+++ Marlin/HAL.h
@@ -147,7 +147,7 @@
   #define TEMP_TIMER_NUM 3
   #define TEMP_TIMER_COUNTER TC1
   #define TEMP_TIMER_CHANNEL 0
-  #define TEMP_FREQUENCY 2000
+  #define TEMP_FREQUENCY 4000
   #define TEMP_TIMER_IRQN TC3_IRQn
   #define HAL_TEMP_TIMER_ISR 	void TC3_Handler()
 
